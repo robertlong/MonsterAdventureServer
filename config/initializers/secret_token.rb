5f4d7cebe707ddb106e96ee294876e622c9491e6
Robert Long <robert@robertlong.me>
2013-01-26 10:48:19 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonsterAdventureServer::Application.config.secret_token = '811377890b95f54fc8c969528238834b2954d515c264a3793de463f591cbca23bac42ef780edf3882efee5fcd2bbe491967711cdc2a1787f40157a45e0e1886e'
